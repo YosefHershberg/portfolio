2b3c8774951aedbc21b0f13cca2ad0f4dfeffea2
Yosef Hershberg <berioo2000@gmail.com>
2024-08-25 13:22:30 +0300

    add

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -54,7 +54,7 @@ export default function ContactForm() {
   return (
     <div className="w-full mx-auto rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <form className="my-2" onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-6">
+        <LabelInputContainer className="mb-6">
           <Label className="mb-2 block" htmlFor="email">Your Email</Label>
           <Input
             {...register("email")}
@@ -65,8 +65,8 @@ export default function ContactForm() {
             placeholder="thedude@abides.com"
           />
           {errors.email && <p className="text-red-600">{errors.email.message}</p>}
-        </div>
-        <div className="mb-6">
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-6">
           <Label className="mb-2 block" htmlFor="name">Your Name</Label>
           <Input
             {...register("name")}
@@ -77,8 +77,8 @@ export default function ContactForm() {
             placeholder="The Dude"
           />
           {errors.name && <p className="text-red-600">{errors.name.message}</p>}
-        </div>
-        <div className="mb-6">
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-6">
           <Label className="mb-2 block" htmlFor="message">Your Message</Label>
           <Textarea
             {...register("message")}
@@ -89,7 +89,7 @@ export default function ContactForm() {
             rows={4}
           />
           {errors.message && <p className="text-red-600">{errors.message.message}</p>}
-        </div>
+        </LabelInputContainer>
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -98,6 +98,7 @@ export default function ContactForm() {
         >
           {isSubmitting ? "Sending..." : "Send"}
         </button>
+        <BottomGradient />
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
       </form>
