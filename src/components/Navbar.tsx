0b91c86bf44897283b1ddde21b3004c44f43cf50
Yosef Hershberg <berioo2000@gmail.com>
2025-05-25 13:33:54 +0300

    removed button import from nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { logo } from "@/assets";
-import { Button } from "./ui/button";
+// import { Button } from "./ui/button";
 
 const Navbar = () => {
 
